43603974702f7f3af630c40b5bcf60920a99ddfc
Diego Saenz <41396886+dssaenzml@users.noreply.github.com>
2023-12-11 13:19:00 +0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -6,6 +6,7 @@ import os
 
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 import mlflow
@@ -36,7 +37,9 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    return
+    X, y = df[['Preagnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
