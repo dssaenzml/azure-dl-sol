240c8fa4912f0146e647dde10912b2e85033275e
Diego Saenz <41396886+dssaenzml@users.noreply.github.com>
2023-12-11 16:11:34 +0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -13,6 +13,7 @@ import mlflow
 
 # define functions
 def main(args):
+    
     # TO DO: enable autologging
     mlflow.autolog()
 
@@ -37,11 +38,18 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X, y = df[[
+        'Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
+        'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
+        'Age'
+    ]].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0
+    )
     return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
+    
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
@@ -64,6 +72,7 @@ def parse_args():
 
 # run script
 if __name__ == "__main__":
+
     # add space in logs
     print("\n\n")
     print("*" * 60)
